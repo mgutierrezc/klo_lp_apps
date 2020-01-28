2aa840d3cfc2b1423c7dbead5e37c291e386f2a3
MARCO ANTONIO GUTIERREZ CHAVEZ <ma.gutierrezch@up.edu.pe>
2020-01-28 09:56:08 -0500

    updated controls

--- controls.py
+++ controls.py
@@ -1,3 +1,9 @@
+doc = """"
+Control parameters for MoneyPolitics
+"""
+
 task_endowments = [9, 15, 15, 15, 25, 25, 40, 80, 125]
-number_of_messages = 1
+poverty_line = task_endowments[3]
 message_cost = 1
+number_of_messages = 1
+possible_tax_systems = [[0, 'Progressivity System'], [1, "Tax Rate System"]]
